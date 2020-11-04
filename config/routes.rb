1402c0a340cdf66486a077e5673437ea6d99a271
Guillaume Canneton <g.canneton@gmail.com>
2020-11-04 15:00:38 +1100

    models update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :airplanes
+  resources :users
+  resources :flights
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
